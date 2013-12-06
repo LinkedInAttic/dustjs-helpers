36901d7846c8aad81f47873b9cb01be458cc9b20
Kate Odnous <kate2753@gmail.com>
2013-12-05 16:44:05 -0800

    Fix spec runner

--- test/jasmine-test/server/specRunner.js
+++ test/jasmine-test/server/specRunner.js
@@ -1,10 +1,10 @@
 var jasmine = require('jasmine-node'),
-    sys = require('util'),
-    path = require('path');
+  sys = require('util'),
+  path = require('path');
 
 /* this should be declared global in order to access them in the spec*/
-dust = require('dustjs-linkedin'),
-dust.helpers = require("../../../lib/dust-helpers").helpers,
+dust = require('dustjs-linkedin');
+dust.helpers = require("../../../lib/dust-helpers").helpers;
 helpersTests = require('../spec/helpersTests');
 
 //Add the tapper helper to test the Tap helper.
@@ -14,7 +14,7 @@ dust.helpers.tapper = function(chunk, context, bodies, params) {
   return chunk;
 };
 
-for(key in jasmine) 
+for(key in jasmine)
   global[key] = jasmine[key];
 
 isVerbose = true;
@@ -35,10 +35,16 @@ process.argv.forEach(function(arg) {
   }
 });
 
-jasmine.executeSpecsInFolder(path.dirname(__dirname) + '/spec', (function(runner, log) {
+var options = [];
+options['specFolders'] = [path.dirname(__dirname) + '/spec'] ;
+options['isVerbose'] = isVerbose;
+options['showColors'] = showColors;
+options['onComplete'] = function(runner, log) {
   if (runner.results().failedCount === 0) {
     return process.exit(0);
   } else {
     return process.exit(1);
   }
-}), isVerbose, showColors);
+};
+
+jasmine.executeSpecsInFolder(options);
