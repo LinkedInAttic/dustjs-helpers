d17d5748bf33cb01a79a01c4681c09bf13071018
Matthew Ouyang <matthew.ouyang@gmail.com>
2014-01-16 00:43:08 -0500

    squash previous commits
    1. GH-32 - populate dust.helpers using iteration
       37d3b84f630a6d34782b03b666ac412adac0bda2
    
    in lib/dust-helpers.js
    
    force a failing script loading order by reworking existing specRunners
    - server - sandwich dust.helpers.tapper assignment between dust and
      dust.helpers assignment
    - client - use LABjs to force a specific script-loading order to achieve
      equivalent effect as the server
    
    2. mv specRunner2.js specRunner.js
       28233cf93480148c3043c0c7d44636fb09a49a2c
    
    3. remove client spec runner
       6b1b3bad1c2e9b5da18ac57cf641a46a726fe318
    
    it's now autogenerated from grunt build + enforcing a specific script load
    order seems like overkill
    
    4. import testUtils
       bde96fefe176838f50c2c4ef5dacb9dc25bbad98

--- test/jasmine-test/server/specRunner.js
+++ test/jasmine-test/server/specRunner.js
@@ -4,15 +4,9 @@ var jasmine = require('jasmine-node'),
 
 //This should be declared global in order to access them in the spec
 dust = require('dustjs-linkedin');
-dust.helpers = require("../../../lib/dust-helpers").helpers;
-
 //Add the tapper helper to test the Tap helper.
 testUtils = require("../../../test/testUtils");
-for(key in testUtils) {
-  dust.helpers[key] = testUtils[key];
-}
-
-//Get unit test specs
+dust.helpers = require("../../../lib/dust-helpers").helpers;
 helpersTests = require('../spec/helpersTests');
 
 for(key in jasmine) {
