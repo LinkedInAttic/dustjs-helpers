ec135591663b8812e28ea8734b6d739c88082eb2
Jimmy Chan <jchan@linkedin.com>
2015-01-14 17:17:33 -0800

    remove if and idx helpers

--- test/jasmine-test/spec/helpersTests.js
+++ test/jasmine-test/spec/helpersTests.js
@@ -14,60 +14,6 @@
     ]
   },
   {
-    name: "if",
-    tests: [
-      {
-        name:     "if helper with no body",
-        source:   '{@if cond="{x}<{y}"/}',
-        context:  { x: 2, y: 3 },
-        expected: "",
-        message: "should test if helper with no body and fail gracefully"
-      },
-      {
-        name:     "if helper with no condition",
-        source:   '{@if}Hello{/if}',
-        context:  { x: 2, y: 3 },
-        expected: "",
-        message: "should test if helper with no condition fails gracefully"
-      },
-      {
-        name:     "if helper without else",
-        source:   '{@if cond="{x}<{y}"}<div> X < Y </div>{/if}',
-        context:  { x: 2, y: 3 },
-        expected: "<div> X < Y </div>",
-        message: "should test if helper without else"
-      },
-      {
-        name:     "if helper with else block",
-        source:   '{@if cond=" \'{x}\'.length && \'{y}\'.length "}<div> X and Y exists </div>{:else}<div> X and Y does not exists </div>{/if}',
-        context:  {},
-        expected: "<div> X and Y does not exists </div>",
-        message: "should test if helper with else block"
-      },
-      {
-        name:     "if helper with else using the or condition",
-        source:   '{@if cond=" \'{x}\'.length || \'{y}\'.length "}<div> X or Y exists </div>{:else}<div> X or Y does not exists </div>{/if}',
-        context:  { x: 1},
-        expected: "<div> X or Y exists </div>",
-        message: "should test if helper with else using the or condition"
-      },
-      {
-        name:     "if helper with else using the and conditon",
-        source:   '{@if cond="( \'{x}\'.length ) && ({x}<3)"}<div> X exists and is 1 </div>{:else}<div> x is not there </div>{/if}',
-        context:  { x : 1},
-        expected: "<div> X exists and is 1 </div>",
-        message: "should test if helper with else usingt he and conditon"
-      },
-      {
-        name:     "if helper using $idx",
-        source:   '{#list}{@if cond="( {$idx} == 1 )"}<div>{y}</div>{/if}{/list}',
-        context:  { x : 1, list: [ { y: 'foo' }, { y: 'bar'} ]},
-        expected: "<div>bar</div>",
-        message: "should test the if helper using $idx"
-      }
-    ]
-  },
-  {
     name: "math",
     tests: [
       {
@@ -1427,25 +1373,6 @@
           context:  { "A": "<html>", "B": "</html>"},
           expected: "{\n  \"A\": \"\\u003chtml>\",\n  \"B\": \"\\u003c/html>\"\n}",
           message: "contextDump simple test"
-      }
-    ]
-  },
-  {
-    name: "idx",
-    tests: [
-      {
-        name:     "idx helper with no body",
-        source:   '{#n}{@idx/}{.} {/n}',
-        context:  { n: ["Mick", "Tom", "Bob"] },
-        expected: "Mick Tom Bob ",
-        message: "idx helper with no body should not render"
-      },
-      {
-        name:     "idx helper within partial included in a array",
-        source:   '{#n}{@idx}{.}>>{/idx}{>hello_there name=. count="30"/}{/n}',
-        context:  { n: ["Mick", "Tom", "Bob"] },
-        expected: "0>>Hello Mick! You have 30 new messages.1>>Hello Tom! You have 30 new messages.2>>Hello Bob! You have 30 new messages.",
-        message: "should test idx helper within partial included in a array"
       }
     ]
   },
